8f6e2a413202a55d90613b23b9e13f5584e64058
Jay T <jaytula@gmail.com>
2020-05-29 18:52:32 -0700

    Write mutation resolvers: write Mutation.login

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -26,6 +26,12 @@ module.exports = {
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
   },
+  Mutation: {
+    login: async (_, {email}, {dataSources}) => {
+      const user = await dataSources.userAPI. findOrCreateUser({email});
+      if(user) return Buffer.from(email).toString('base64');
+    }
+  },
   Mission: {
     missionPatch: (_, { mission, size } = { size: 'LARGE' }) => {
       return size === 'SMALL'
