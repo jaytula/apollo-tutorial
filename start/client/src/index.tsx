480fcc2a09d7433c64c495a02c1992bcb07419b1
Jay T <jaytula@gmail.com>
2020-06-01 13:45:45 -0700

    Connecting your API to a client: Make your first query

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,6 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -11,3 +12,18 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link,
 });
+
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
