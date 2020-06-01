5df2f9225e8c296ef9bec534e60eeba83a06fe00
Jay T <jaytula@gmail.com>
2020-06-01 13:46:42 -0700

    Revert "Connecting your API to a client: Make your first query"
    This reverts commit 480fcc2a09d7433c64c495a02c1992bcb07419b1.

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,7 +1,6 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -12,18 +11,3 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link,
 });
-
-client
-  .query({
-    query: gql`
-      query GetLaunch {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `,
-  })
-  .then(result => console.log(result));
