4c096181fcc639f2307bf4ba15eb7549497c3960
Jay T <jaytula@gmail.com>
2020-06-04 09:46:14 -0700

    Fetch data with queries: Customizing the fetch policy

--- start/client/src/pages/profile.tsx
+++ start/client/src/pages/profile.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import gql from 'graphql-tag';
+import { LAUNCH_TILE_DATA } from './launches';
+import * as GetMyTripsTypes from './__generated__/GetMyTrips';
+import { useQuery } from '@apollo/react-hooks';
+import { Loading, Header, LaunchTile } from '../components';
 
 interface ProfileProps extends RouteComponentProps {}
 
+export const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 const Profile: React.FC<ProfileProps> = () => {
-  return <div />;
-}
+  const { loading, error, data } = useQuery<GetMyTripsTypes.GetMyTrips, any>(
+    GET_MY_TRIPS,
+    {
+      fetchPolicy: 'network-only',
+    }
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (data === undefined) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch}></LaunchTile>
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
+};
 
 export default Profile;
