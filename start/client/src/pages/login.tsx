c4349f70c6c4543ff3ede3633cc794d447015835
Jay T <jaytula@gmail.com>
2020-06-04 18:10:13 -0700

    Update data with mutations: Update data with useMutation

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React from "react";
+import gql from "graphql-tag";
+import * as LoginTypes from "./__generated__/login";
+import { useMutation } from "@apollo/react-hooks";
+import { LoginForm } from "../components";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation<
+    LoginTypes.login,
+    LoginTypes.loginVariables
+  >(LOGIN_USER);
+  return <LoginForm login={login} />;
 }
