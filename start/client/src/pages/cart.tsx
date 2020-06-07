8260150fa802bae843b33b5149b41b13ec6c3d57
Jay T <jaytula@gmail.com>
2020-06-07 13:00:14 -0700

    Manage local state: Query local data - Cart

--- start/client/src/pages/cart.tsx
+++ start/client/src/pages/cart.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import gql from "graphql-tag";
+import * as GetCartItemsTypes from "./__generated__/GetCartItems";
+import { useQuery } from "@apollo/react-hooks";
+import { Loading, Header } from "../components";
+import { CartItem, BookTrips } from "../containers";
 
-interface CartProps extends RouteComponentProps {
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
 
-}
+interface CartProps extends RouteComponentProps {}
 
 const Cart: React.FC<CartProps> = () => {
-  return <div />;
-}
+  const { loading, error, data } = useQuery<GetCartItemsTypes.GetCartItems>(
+    GET_CART_ITEMS
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <Fragment>
+      <Header>My Cart</Header>
+
+      {!data || (!!data && data.cartItems.length === 0) ? (
+        <p data-testid="empty-message">No items in your cart</p>
+      ) : (
+        <Fragment>
+          {!!data &&
+            data.cartItems.map((launchId: any) => (
+              <CartItem key={launchId} launchId={launchId} />
+            ))}
+          <BookTrips cartItems={!!data ? data.cartItems : []} />
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 export default Cart;
