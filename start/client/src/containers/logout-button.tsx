63a0797c6cbd92352240f4a2173f7b785b98d8e9
Jay T <jaytula@gmail.com>
2020-06-08 10:21:04 -0700

    Manage local state: Direct cache writes - Logout Button

--- start/client/src/containers/logout-button.tsx
+++ start/client/src/containers/logout-button.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useApolloClient } from '@apollo/react-hooks';
+import styled from 'react-emotion';
+import { menuItemClassName } from '../components/menu-item';
+import { ReactComponent as ExitIcon} from '../assets/icons/exit.svg';
 
 const LogoutButton: React.FC<any> = () => {
-  return <div/>;
+  const client = useApolloClient();
+  return <StyledButton onClick={() => {
+    client.writeData({ data: {isLoggedIn: false}});
+    localStorage.clear();
+  }}>
+    <ExitIcon />
+    Logout
+  </StyledButton>
 }
+
+const StyledButton = styled('button')(menuItemClassName, {
+  background: 'none',
+  border: 'none',
+  padding: 0
+})
 
 export default LogoutButton;
